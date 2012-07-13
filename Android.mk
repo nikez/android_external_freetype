6edad257ffcb8bdf60baa9f1f03593ac96ecb1b0
Nikez <nikez141@gmail.com>
2012-07-13 13:53:08 +0200

    ICS-INIT

---
+++ Android.mk
@@ -0,0 +1,65 @@
+# this is now the default FreeType build for Android
+#
+ifndef USE_FREETYPE
+USE_FREETYPE := 2.4.2
+endif
+
+ifeq ($(USE_FREETYPE),2.4.2)
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# compile in ARM mode, since the glyph loader/renderer is a hotspot
+# when loading complex pages in the browser
+#
+LOCAL_ARM_MODE := arm
+
+LOCAL_SRC_FILES:= \
+	src/base/ftbbox.c \
+	src/base/ftbitmap.c \
+	src/base/ftfstype.c \
+	src/base/ftglyph.c \
+	src/base/ftlcdfil.c \
+	src/base/ftstroke.c \
+	src/base/fttype1.c \
+	src/base/ftxf86.c \
+	src/base/ftbase.c \
+	src/base/ftsystem.c \
+	src/base/ftinit.c \
+	src/base/ftgasp.c \
+	src/raster/raster.c \
+	src/sfnt/sfnt.c \
+	src/smooth/smooth.c \
+	src/autofit/autofit.c \
+	src/truetype/truetype.c \
+	src/cff/cff.c \
+	src/psnames/psnames.c \
+	src/pshinter/pshinter.c
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/builds \
+	$(LOCAL_PATH)/include
+
+LOCAL_CFLAGS += -W -Wall
+LOCAL_CFLAGS += -fPIC -DPIC
+LOCAL_CFLAGS += "-DDARWIN_NO_CARBON"
+LOCAL_CFLAGS += "-DFT2_BUILD_LIBRARY"
+LOCAL_CFLAGS += -fno-strict-aliasing
+
+# the following is for testing only, and should not be used in final builds
+# of the product
+#LOCAL_CFLAGS += "-DTT_CONFIG_OPTION_BYTECODE_INTERPRETER"
+
+# Freetype can't be built without optimizations, so we enforce -O2 if no
+# other optimization flag is set - but we don't override what the global
+# flags are saying if something else is given (-Os or -O3 are useful)
+ifeq ($(findstring -O, $(TARGET_GLOBAL_CFLAGS)),)
+LOCAL_CFLAGS += -O2
+endif
+ifneq ($(findstring -O0, $(TARGET_GLOBAL_CFLAGS)),)
+LOCAL_CFLAGS += -O2
+endif
+
+LOCAL_MODULE:= libft2
+
+include $(BUILD_STATIC_LIBRARY)
+endif
